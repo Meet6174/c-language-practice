0fbd2a056c3433836ec3f2d60d155ddce6642ae1
Meet6174 <jairamdesai18184@gmail.com>
2025-01-25 22:44:17 +0530

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a=1;
+    float b=0.1;
+    char c='meet';
+    printf("the val of a is = %d and the val of b is = %f and the val of c = %c",a,b,c);
+    return 0;
+}
