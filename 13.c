6489b5ee18d2c866e11802c67344536f5facd7c6
Meet6174 <jairamdesai18184@gmail.com>
2025-01-29 00:47:46 +0530

    Add files via upload

---
+++ 13.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    switch (a)
+    {
+        case 1:
+        {
+            printf("your num is 1");
+        }
+        break;
+        case 2:
+        {
+            printf("your num is 2");
+        }
+        
+    }
+    return 0;
+}
