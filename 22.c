feb27df2abf519b9448dd7b57f80280aa94ead44
Meet6174 <jairamdesai18184@gmail.com>
2025-02-04 23:47:03 +0530

    Add files via upload

---
+++ 22.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int myage=430;
+    if(myage>60)
+    {
+        printf(" you are seniar citiezen");
+    }
+    else if(myage>50)
+    {
+        printf(" you are  citiezen");
+    }
+    else if(myage>40)
+    {
+        printf(" you citiezen");
+    }
+   else
+    {
+        printf(" citiezen");
+    }
+    return 0;
+}
