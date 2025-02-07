bd9d9f9ba7abd74c9f4f7994c339d282608c3af2
Meet6174 <jairamdesai18184@gmail.com>
2025-02-07 21:32:59 +0530

    Add files via upload

---
+++ 26.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char st;
+gets(st);//with gets you can enter the space
+printf("%s",st);
+puts(st);//after using the puts cursor will go in new line thats diffrent between puts and printf
+int n=strlrn(st);
+printf("the lenth of string is %d",n);
+    return 0;
+}
