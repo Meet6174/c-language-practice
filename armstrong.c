213ee4c67a2b095b3dcc368cbc409981d02d6541
Meet6174 <jairamdesai18184@gmail.com>
2025-02-10 22:42:18 +0530

    Add files via upload

---
+++ armstrong.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e,f,num;
+    for (num=0;num<999;num++)
+    {
+    a = num%100;
+    b= a%10;
+    c = (num-a)/100;
+    d = (a-b)/10;
+    f = b*b*b + c*c*c + d*d*d; 
+    if(num == f)
+    {
+        printf("%d.\n",num);
+    }  
+    } 
+    return 0;
+}
