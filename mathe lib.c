0169b046a45eb62ccc64aec5b8891acb6f9a3ac6
Meet6174 <jairamdesai18184@gmail.com>
2025-03-01 22:22:57 +0530

    Add files via upload

---
+++ mathe lib.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a = 5;
+    printf("the val of a squre is %f",pow(a,2));// %f is important becaus pow fn retuns the float val
+    return 0;
+}
