0b5428732c08d7b6c389b0ef1913dd5cce4eb57f
Meet6174 <jairamdesai18184@gmail.com>
2025-03-04 00:02:04 +0530

    Add files via upload

---
+++ prime num test.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+     int a,i,g;
+     scanf("%d",&a);
+    
+     for(i=2;i<a;i++)
+     {
+        if(a%i==0)
+        {
+            g = i;
+            printf("a is div by %d\n",i);
+            printf("this num is not prime num\n");
+        }
+      
+     }
+     if(g ==0)
+     {
+        printf("this num is prime num");
+     }
+     getch();
+     return 0;
+
+}
