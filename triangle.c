92335d45186329fd3165a0d2db60b57bfac7ca9f
Meet6174 <jairamdesai18184@gmail.com>
2025-03-09 22:01:45 +0530

    Add files via upload

---
+++ triangle.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, k, a;
+    j = 0;
+    for (i = 0; i < 10; i++)
+    {
+        a = j + 2;
+        switch (i)
+        {
+        case 0:
+            printf(" ");
+        case 1:
+            printf(" ");
+        case 2:
+            printf(" ");
+        case 3:
+            printf(" ");
+        case 4:
+            printf(" ");
+        case 5:
+            printf(" ");
+        case 6:
+            printf(" ");
+        case 7:
+            printf(" ");
+        case 8:
+            printf(" ");
+        case 9:
+            printf(" ");
+        }
+        for (j = 0; j < a; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
