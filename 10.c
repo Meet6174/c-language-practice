65ac4268219bcaeb9487773348b96c973b6cf99a
Meet6174 <jairamdesai18184@gmail.com>
2025-03-14 21:17:45 +0530

    10.c

--- 10.c
+++ 10.c
@@ -2,12 +2,15 @@
 int main()
 {
     int a,b,c;
+    printf("enter the val of a :\n");
     scanf("%d",&a);
+    printf("enter the val of b :\n");
     scanf("%d",&b);
     c=0;
     c = a;
     a = b;
     b = c;
+    printf("after the swaping of val \n");
     printf("the val of a %d.\n",a);
     printf("the val of b %d.\n",b);
     return 0;
