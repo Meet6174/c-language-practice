4ed2d0b1019cfa73405c05a9eb41228e14147a84
Meet6174 <jairamdesai18184@gmail.com>
2025-03-15 23:12:06 +0530

    6.c

--- 6.c
+++ 6.c
@@ -3,6 +3,8 @@ int main()
 {
   int a=2;
   float b=2.28;
-  printf("the val of a+b =%.3f",a+b);
+  printf("the value of a =%.7f",a);
+  printf("the value of b =%.7f",b);
+  printf("the value of a+b =%.3f",a+b);
   return 0;
 }
