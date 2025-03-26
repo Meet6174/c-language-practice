4fab84aeb9629f4f10c699d4e8c1fb895523c6de
Meet6174 <jairamdesai18184@gmail.com>
2025-03-26 23:51:29 +0530

    26.c

--- 26.c
+++ 26.c
@@ -7,6 +7,6 @@ gets(st);//with gets you can enter the space
 printf("%s",st);
 puts(st);//after using the puts cursor will go in new line thats diffrent between puts and printf
 int n=strlrn(st);
-printf("the lenth of string is %d",n);
+printf("the lenth of string is = %d",n);
     return 0;
 }
